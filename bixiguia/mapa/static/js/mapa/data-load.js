33c61b90776aeccecdb9b6b32f2a2269ff66da7d
Rafael Augusto <p.rcaugusto@gmail.com>
2011-10-11 19:46:10 -0300

    Adição de InfoWindow com nome e endereço para cada marcador no mapa

--- bixiguia/mapa/static/js/mapa/data-load.js
+++ bixiguia/mapa/static/js/mapa/data-load.js
@@ -1,17 +1,38 @@
+/**
+ * Mostra os pontos no mapa
+ */
 function show_data(map, data) {
-    for (var i=0; i<data.length; i++) {
+    
+    // para cada ponto existente no banco
+    for (var i = 0; i < data.length; i++) {
 
+        // pega informações referentes ao local
         local = data[i];
+        
+        // cria o marcador referente ao ponto
+        marker = new google.maps.Marker({
+            'position': new google.maps.LatLng(
+                local.fields.latitude,
+                local.fields.longitude
+            ),
+            'map': map,
+            'title': local.fields.nome
+        });
+        
+        // adiciona listener para abrir janela quando o marker for clicado
+        (function(marker, local){
+            google.maps.event.addListener(marker, 'click', function(){
+                infowindow = new google.maps.InfoWindow({
+                    content: '<font size=3><b>' + local.fields.nome + '</b>\n\<br /></font>' 
+                        + '<font size=2>' + local.fields.endereco + '</font>',
+                    size: new google.maps.Size(50,50),
+                    position: marker.position
 
-        marker = new google.maps.Marker(
-                    {
-                        'position': new google.maps.LatLng(
-                            local.fields.latitude,
-                            local.fields.longitude
-                        ),
-                        'map': map,
-                        'title': local.fields.nome
-                    });
+                });
+                infowindow.open(map);
+            });
+        })(marker, local);
+        
     }
 };
 
