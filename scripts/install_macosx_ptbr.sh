0b8823125fbefc54be96b35e64f90800bf742da7
Rafael Augusto <p.rcaugusto@gmail.com>
2011-11-20 22:09:15 -0200

    Adicionando scripts de instalação de requisitos

---
+++ scripts/install_macosx_ptbr.sh
@@ -0,0 +1,30 @@
+# Installs Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+
+# Installs wget
+brew install wget
+
+# Changes to Downloads directory - OS X language should be currently English
+cd ~/Transferências
+
+# Downloads Setuptools' egg
+wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11-py2.7.egg
+
+# Shells Setuptools' egg
+sh setuptools-0.6c11-py2.7.egg
+
+# Installs pip
+easy_install pip
+
+# Installs libjpg, imagemagick and all dependencies (for treating images)
+brew install libjpg
+brew install imagemagick
+
+# Installs Django
+pip install django
+
+# Installs postgresql
+brew install postgresql
+
+# Creates Database bixiguia
+createdb bixiguia
