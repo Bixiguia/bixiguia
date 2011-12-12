37d1513294d59ce95de8cb3f4bbaf47f4a357107
Rafael Augusto <p.rcaugusto@gmail.com>
2011-12-12 14:23:28 -0200

    Adicionando help texts nos campos de inserção de locais

--- bixiguia/guia/models/__init__.py
+++ bixiguia/guia/models/__init__.py
@@ -8,7 +8,7 @@ from bixiguia.guia.models import mixins
 
 
 class Categoria(mixins.DataMixin):
-    nome = models.CharField(max_length=64)
+    nome = models.CharField(max_length=64, help_text=u'Ex.: Gastronomia')
     nome_slug = models.SlugField(max_length=64, unique=True,
                     verbose_name=u'Nome na URL')
     visivel = models.BooleanField(default=True, verbose_name=u'Visível')
@@ -24,27 +24,38 @@ class Categoria(mixins.DataMixin):
 
 
 class Local(mixins.DataMixin):
-    nome = models.CharField(max_length=64, verbose_name=u'Nome do local')
+    nome = models.CharField(max_length=64, verbose_name=u'Nome do local',
+                    help_text=u'Ex.: Cantina C Q Sabe')
     nome_slug = models.SlugField(max_length=64, unique=True,
-                    verbose_name=u'Nome na URL')
+                    verbose_name=u'Nome na URL', 
+                    help_text=u'Nome que irá aparecer na barra de endereços.<br>Ex.: Cantina C Q Sabe')
 
     categoria = models.ForeignKey(Categoria)
 
-    endereco = models.CharField(max_length=64, verbose_name=u'Endereço')
-    complemento = models.CharField(max_length=16, blank=True)
+    endereco = models.CharField(max_length=64, verbose_name=u'Endereço',
+                    help_text=u'Ex.: R. Conselheiro Ramalho, 800')
+    complemento = models.CharField(max_length=16, blank=True, 
+                    help_text=u'Ex.: 14')
 
-    latitude = models.DecimalField(max_digits=10, decimal_places=8)
-    longitude = models.DecimalField(max_digits=10, decimal_places=8)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, 
+                    help_text=u'Ex.: -23.50')
+    longitude = models.DecimalField(max_digits=10, decimal_places=8,
+                    help_text=u'Ex.: -46.50')
 
     ano_fundacao = models.PositiveIntegerField(null=True, blank=True,
-                        verbose_name=u'Ano de fundação')
-    descricao = models.TextField(verbose_name=u'Descrição', blank=True)
+                        verbose_name=u'Ano de fundação', 
+                        help_text=u'Ex.: 1950')
+    descricao = models.TextField(verbose_name=u'Descrição', blank=True,
+                    help_text=u'Escreva um texto descritivo com cerca de 500 caracteres sobre o local.')
     horario_funcionamento = models.CharField(max_length=128, blank=True,
-                                verbose_name=u'Horário de funcionamento')
-
-    site = models.URLField(blank=True, verify_exists=False)
-    contatos = models.CharField(max_length=32, blank=True)
-    email = models.EmailField(blank=True)
+                                verbose_name=u'Horário de funcionamento',
+                                help_text=u'Ex.: Segunda a Quinta das 11h30 às 02h00')
+                                
+    site = models.URLField(blank=True, verify_exists=False, 
+                    help_text=u'Ex.: http://www.bixiguia.com.br')
+    contatos = models.CharField(max_length=32, blank=True,
+                    help_text=u'Ex.: (11) 1234-5678')
+    email = models.EmailField(blank=True, help_text=u'Ex.: bixiguia@bixiguia.com.br')
     visivel = models.BooleanField(default=True, verbose_name=u'Visível')
 
     def __unicode__(self):
